44a2929d6890ce7e9205341bb6c553152ead8a8f
jgy4419 <jgy_98@naver.com>
2022-02-02 17:15:10 +0900

    마트 마커 생성

--- src/components/martLocation/KakaoMap.vue
+++ src/components/martLocation/KakaoMap.vue
@@ -9,6 +9,9 @@ export default {
         return{
             logoImg: "https://user-images.githubusercontent.com/76980526/148074503-94784be9-47a4-41cc-9fa5-bf88c79b2b18.jpeg",
             martLocation: [],
+            martTitle: '',
+            abc: 'abc',
+
         }
     },
     computed: {
@@ -47,11 +50,18 @@ export default {
             var imageSrc = this.logoImg,
             imageSize = new kakao.maps.Size(64, 69),
             imageOption = {offset: new kakao.maps.Point(27, 69)};
-
+            
+            
             for(var i = 0; i < location1.length; i++){
                 let positions = 
                 {
-                    title: martTitle[i],
+                    // title: `<div>${martTitle[i]}</div>`,
+                    title: `<div style = "
+                    padding: 3px;
+                    color: #333;
+                    font-weight: 500;
+                    font-size: 18px;
+                    ">${martTitle[i]}</div>`,
                     latlng: new kakao.maps.LatLng(location1[i], location2[i])
                 }
 
@@ -65,14 +75,41 @@ export default {
                     map: map, // 마커를 표시할 지도
                     position: positions.latlng, // 마커를 표시할 위치
                     title : positions.title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                    image : markerImage // 마커 이미지 
+                    image : markerImage, // 마커 이미지 
+                    clickable: true,
                 });
+                
+                var infoWindow = new kakao.maps.InfoWindow({
+                    content: positions.title
+                });
+
+                kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infoWindow));
+                kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infoWindow));;
+            }
+            // var customOverlay = new kakao.maps.CustomOverlay({
+            //         position: marker.position,
+            //         content: positions.title
+            //     })
+            // customOverlay.setMap(map);
+            // 인포윈도우를 표시하는 클로저를 만드는 함수.
+            function makeOverListener(map, marker, infowindow){
+                return function(){
+                    infowindow.open(map, marker);
+                };
+            }
+            // 인포윈도우를 닫는 클로저를 만드는 함수.
+            function makeOutListener(infowindow){
+                return function(){
+                    infowindow.close();
+                }
             }
         },
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+.martTitle{
+    background-color: #333;
+}
 </style>
